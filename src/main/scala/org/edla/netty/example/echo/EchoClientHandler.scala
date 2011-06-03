7ff53af649cfd6c7214c80aa57f24fc5ab8bb3fc
Olivier ROLAND <olivier.roland@edla.org>
2011-06-03 19:22:31 +0200

    Echo client and server

---
+++ src/main/scala/org/edla/netty/example/echo/EchoClientHandler.scala
@@ -0,0 +1,60 @@
+package org.edla.netty.example.echo
+
+import java.util.concurrent.atomic.AtomicLong
+import org.jboss.netty.buffer.{ ChannelBuffer, ChannelBuffers }
+import org.jboss.netty.channel.{
+  Channel,
+  ChannelEvent,
+  ChannelHandlerContext,
+  ChannelState,
+  ChannelStateEvent,
+  ExceptionEvent,
+  MessageEvent,
+  SimpleChannelUpstreamHandler,
+  WriteCompletionEvent
+}
+import java.util.logging.Logger
+import util.control.Breaks._
+
+/**
+ * Handler implementation for the echo client.  It initiates the ping-pong
+ * traffic between the echo client and server by sending the first message to
+ * the server.
+ */
+class EchoClientHandler(firstMessageSize: Int) extends SimpleChannelUpstreamHandler {
+
+  require(firstMessageSize > 0)
+
+  private val logger = Logger.getLogger(getClass.getName)
+
+  private val transferredBytes = new AtomicLong
+
+  private val firstMessage = ChannelBuffers.buffer(firstMessageSize)
+  val range = 0.until(firstMessage.capacity())
+  for (i <- range) { firstMessage.writeByte(i.toByte) }
+
+  def getTransferredBytes: Long = {
+    transferredBytes.get
+  }
+
+  override def channelConnected(ctx: ChannelHandlerContext, e: ChannelStateEvent): Unit = {
+    // Send the first message.  Server will not send anything here
+    // because the firstMessage's capacity is 0.
+    e.getChannel().write(firstMessage)
+  }
+
+  override def messageReceived(ctx: ChannelHandlerContext, e: MessageEvent): Unit = {
+    // Send back the received message to the remote peer.
+    transferredBytes.addAndGet((e.getMessage match {
+      case c: ChannelBuffer => c
+      case _ => throw new ClassCastException
+    }) readableBytes)
+    e.getChannel().write(e.getMessage());
+  }
+
+  override def exceptionCaught(context: ChannelHandlerContext, e: ExceptionEvent): Unit = {
+    // Close the connection when an exception is raised.
+    logger.warning("Unexpected exception from downstream." + e.getCause)
+    e.getChannel.close
+  }
+}
---
+++ src/main/scala/org/edla/netty/example/echo/EchoServerHandler.scala
@@ -0,0 +1,43 @@
+package org.edla.netty.example.echo
+
+import java.util.concurrent.atomic.AtomicLong
+import org.jboss.netty.buffer.ChannelBuffer
+import org.jboss.netty.channel.{
+  ChannelEvent,
+  ChannelHandlerContext,
+  ChannelStateEvent,
+  ExceptionEvent,
+  MessageEvent,
+  SimpleChannelUpstreamHandler
+}
+import java.util.logging.Logger
+
+/**
+ * Handler implementation for the echo server.
+ */
+class EchoServerHandler extends SimpleChannelUpstreamHandler {
+
+  val logger = Logger.getLogger(getClass.getName)
+
+  val transferredBytes = new AtomicLong
+
+  def getTransferredBytes: Long = {
+    transferredBytes.get
+  }
+
+  override def messageReceived(ctx: ChannelHandlerContext, e: MessageEvent): Unit = {
+    // Send back the received message to the remote peer.
+    transferredBytes.addAndGet((e.getMessage match {
+      case c: ChannelBuffer => c
+      case _ => throw new ClassCastException
+    }) readableBytes)
+    e.getChannel().write(e.getMessage())
+  }
+
+  override def exceptionCaught(context: ChannelHandlerContext, e: ExceptionEvent): Unit = {
+    // Close the connection when an exception is raised.
+    logger.warning("Unexpected exception from downstream." + e.getCause)
+    e.getChannel.close
+  }
+
+}
